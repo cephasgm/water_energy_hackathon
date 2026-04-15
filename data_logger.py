ba67056d2c041896ef67fef6a8903df0d30cda67
CephasGM <cephas.mkama@gmail.com>
2026-04-15 03:19:09 +0300

    Create data_logger.py

---
+++ data_logger.py
@@ -0,0 +1,20 @@
+# data_logger.py
+import csv
+import os
+from config import DATA_LOG_FILE
+
+class DataLogger:
+    def __init__(self, filename=DATA_LOG_FILE):
+        self.filename = filename
+        self.fieldnames = ["timestamp", "water_level_l", "outflow_lps", "pressure_bar",
+                           "pump_running", "energy_kwh_total", "decision_reason",
+                           "tariff_rate", "leak_flag"]
+        if not os.path.exists(filename):
+            with open(filename, 'w', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=self.fieldnames)
+                writer.writeheader()
+
+    def log(self, data_dict):
+        with open(self.filename, 'a', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=self.fieldnames)
+            writer.writerow(data_dict)
